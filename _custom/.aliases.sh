8f2c0b7fb277fab97cd0bbd652dbb9a560fef180
NicolasBizzozzero <nicolas.bizzozzero@protonmail.com>
2019-12-01 18:38:03 +0100

    Updated aliases.sh

--- _custom/.aliases.sh
+++ _custom/.aliases.sh
@@ -3,9 +3,14 @@ alias explorer="$EXPLORER"
 alias ls='ls --color --human-readable --literal'
 alias la='ls -A'
 alias ll='ls -lA'
+alias lt='ls -t'
+
+alias mkdir='mkdir -p'
 
 alias mount='mount | column -t'
 
 alias lp='lp -o orientation-requested=3 -o print-quality=5 -o sides=two-sided-long-edge'
+
+alias subl='subl3'
 
 #alias pdflatex='pdflatex -output-directory=bin'
